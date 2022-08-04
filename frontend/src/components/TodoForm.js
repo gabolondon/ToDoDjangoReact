e9cbe575713c15f2a827d3a2b06733ab1dc66e6a
gabo Londono <ing.gabriel.londono@gmail.com>
2022-08-03 19:25:06 -0500

    pagina TODO funcionandoal 100%

--- frontend/src/components/TodoForm.js
+++ frontend/src/components/TodoForm.js
@@ -3,20 +3,37 @@ import Form from 'react-bootstrap/Form'
 import Button from "react-bootstrap/Button";
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
+import axios from "axios";
 
-export default function TodoForm() {
+export default function TodoForm({todos, setTodos}) {
     const [name, setName] = useState('');
 
+    const handleSubmit= evento=>{
+        evento.preventDefault();
+        if(!name){
+            alert("por favor ingrese un nombre correcto");
+            return;
+        }
+        axios.post(`api/todos/`, { 
+            name: name 
+            }).then((res) =>{
+            setName("");
+            const { data }= res;
+            setTodos([
+                ...todos,
+                data
+            ])    
+            }).catch(()=>{alert("algo salio mal agregando todo")})
+    }
     const handleChange = evento => {
         setName(evento.target.value);
     }
-    return <Form>
+    return <Form onSubmit={handleSubmit}>
         <InputGroup className='mb-3'>
             <FormControl 
                 placeholder='Inserte un nuevo Todo'
                 onChange= {handleChange}
                 value = {name}
-            
             />
             <Button type='submit'>Add</Button>
         </InputGroup>
